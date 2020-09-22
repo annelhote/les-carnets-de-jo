42ee818529c734327e0d6453a7b28f3d3e3ad095
Anne L'Hôte <anne.lhote@gmail.com>
2020-09-22 22:34:59 +0200

    Add menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" title="Les carnets de Jo">
+    <div class="text-center">
+      <img src="./assets/logo.png" title="Les carnets de Jo">
+    </div>
+    <div class="app__nav text-center">
+      <b-nav align="center">
+        <b-nav-item>Accueil</b-nav-item>
+        <b-nav-item>Qui sommes nous ?</b-nav-item>
+        <b-nav-item>Nos actions</b-nav-item>
+        <b-nav-item>Nos outils</b-nav-item>
+        <b-nav-item>Rejoignez-nous</b-nav-item>
+        <b-nav-item>Contact</b-nav-item>
+      </b-nav>
+    </div>
   </div>
 </template>
 
@@ -9,3 +21,9 @@ export default {
   name: 'app'
 }
 </script>
+
+<style scoped>
+.app__nav {
+  background-color: #fdc500;
+}
+</style>
