34f066814a500686b0f319597738f752f9617fe4
Anne L'Hôte <anne.lhote@gmail.com>
2020-09-22 23:54:48 +0200

    Config the deployment

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:annelhote/sans-filtre.git master:gh-pages
+git push -f git@github.com:annelhote/les-carnets-de-jo.git master:gh-pages
 
 cd -
