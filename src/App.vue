942b8fa8f5a7fa21e0282cbfa0999714020fb27c
Anne L'Hôte <anne.lhote@gmail.com>
2021-01-12 20:52:16 +0100

    Updates

--- src/App.vue
+++ src/App.vue
@@ -36,11 +36,6 @@
           </a>
         </b-nav-item>
         <b-nav-item>
-          <a @click="goTo('section_quote')">
-            Citation
-          </a>
-        </b-nav-item>
-        <b-nav-item>
           <a @click="goTo('section_join')">
             Nous soutenir
           </a>
@@ -76,8 +71,7 @@
       </div>
     </section>
     <section class="app__section app__section__even row bg-warning justify-content-md-center" ref="section_why">
-      <div class="col-6">
-        <img src="./assets/fencer_girl_in_wheelchair.png" alt="Escrimeuse en fauteuil roulant" class="app__section__wheelchair" width="300">
+      <div class="col-12">
         <div class="app__section__title text-uppercase text-primary text-center">
           Pourquoi ?
         </div>
@@ -91,10 +85,27 @@
           renforcer l’égalité des chances et la cohésion sociale, l'association a pour vocation de développer
           l'impact éducatif du sport et s’adresse à la jeunesse pour lui donner envie de bouger mais aussi
           d’apprendre, de créer, de s’éveiller et de grandir grâce au sport.<br><br>
-          Conscients que le sport ne peut pas tout résoudre, qu'il n'est ni magique pour la société,
-          ni éducatif par nature, nous faisons le pari qu'avec de l'énergie à revendre, un réseau de professionnels
-          et d'experts, des outils innovants et des expérimentations sur le terrain, nous participerons à révéler
-          son potentiel éducatif comme moyen au service de l'éducation de demain.
+          Conscients que le sport ne peut pas tout résoudre, qu'il n'est ni magique pour la société, ni éducatif par
+          nature, nous faisons le pari qu'avec de l'énergie positive, un réseau expérimenté, des outils innovants et des
+          expérimentations sur le terrain, nous participerons à révéler son potentiel éducatif comme moyen au service de
+          l'éducation de demain.
+        </div>
+      </div>
+    </section>
+    <section class="app__section app__section__odd row justify-content-md-center bg-warning" ref="section_quote">
+      <div class="col-6 text-center">
+        <div class="app__section__image mb-5">
+          <font-awesome-icon icon="quote-right" size="10x"></font-awesome-icon>
+        </div>
+        <div class="app__section__content font-weight-bold mt-3">
+          « L'éducation est une arme puissante pour faire évoluer les mentalités et transcender les différences,
+          et le sport est une source d'inspiration, de dépassement, de tolérance et d'apprentissage
+          du respect de la jeunesse.<br><br>
+          Ces deux éléments participent à créer
+          une société plus juste et fraternelle.»
+        </div>
+        <div class="app__section__author font-italic mt-3 font-weight-lighter">
+          Nelson Mandela
         </div>
       </div>
     </section>
@@ -107,15 +118,17 @@
           Astrid GUYART, escrimeuse olympique, vice-championne du monde de fleuret, souhaitait offrir un livre racontant
           une belle histoire de sport à sa nièce. Ne le trouvant pas en librairie, elle décide de l’écrire elle-même...
         </div>
-        <div>
+        <div class="mb-3">
           C’est ainsi que la collection de livres jeunesse « Les incroyables rencontres de Jo » est née. Chaque tome
           allie le plaisir des premières lectures avec la découverte d’un grand champion français et d’une valeur du
           sport.
+          <br>
           <br>
           L’enthousiasme rencontré auprès des lecteurs et les sollicitations de plusieurs enseignants lui donnent
           l’envie de mettre plus largement l’univers du sport, de l’olympisme et de ses valeurs au service de
           l’épanouissement et de la réussite des enfants. Elle fonde « Les Carnets de Jo », association d’intérêt
           général dédiée à l’éducation par le sport.
+          <br>
           <br>
           Si l'objectif initial de la collection "Les Incroyables Rencontres de Jo" était de donner envie aux enfants
           de s'épanouir et de grandir grâce à la pratique d'une discipline sportive, l'objectif de l'association "Les
@@ -144,10 +157,11 @@
               </div>
               <div class="mt-2">
                 Escrimeuse olympique multi-médaillée en championnats du Monde et championnats d'Europe, Astrid est
-                aussi ingénieure dans l'industrie aérospatiale et auteure de la collection jeunesse "Les Incroyables
-                Rencontres de Jo". Engagée pour la place et les droits des athlètes, elle est membre des commissions
-                des athlètes du Comité National Olympique et Sportif Français (CNOSF), de Paris 2024 et de l’Agence
-                Française de Lutte contre le Dopage (AFLD).
+                ingénieure dans l'industrie aérospatiale, auteure de la collection jeunesse "Les Incroyables Rencontres
+                de Jo" et ambassadrice Education du Comité National Olympique et Sportif Français (CNOSF). Engagée pour
+                la place et les droits des athlètes, elle est également membre des commissions des athlètes de Paris
+                2024 et du CNOSF, co-présidente du Comité des Sportifs de de l’Agence Française de Lutte contre le
+                Dopage (AFLD) et vice-présidente de l'Association des Olympiens Français (AOF).
               </div>
             </div>
           </div>
@@ -161,10 +175,11 @@
                 Trésorière et co-fondatrice de l'association
               </div>
               <div class="mt-2">
-                Directrice des Relations Institutionnelles du Comité National Olympique et Sportif Français (CNOSF),
-                Julie est engagée pour le place de la jeunesse et l'égalité des chances. Son parcours professionnel
-                en collectivités, à l'Assemblée Nationale et dans les ministères des Sports et de la Ville, lui a
-                apporté une connaissance fine des enjeux et des solutions terrains.
+                Responsable des affaires publiques dans le digital et ancienne directrice des Relations Institutionnelles
+                du Comité National Olympique et Sportif Français (CNOSF), Julie est engagée pour le place de la jeunesse
+                et l'égalité des chances. Son parcours professionnel en collectivités, à l'Assemblée Nationale et dans
+                les ministères des Sports et de la Ville, lui a apporté une connaissance fine des enjeux et des solutions
+                terrains.
               </div>
             </div>
           </div>
@@ -176,9 +191,13 @@
         <div class="app__section__title text-uppercase text-primary">
           Nos actions
         </div>
-        <div class="app__section__subtitle font-weight-bold">
+        <div class="app__section__subtitle font-weight-bold mb-4">
           Nous intervenons auprès d’élèves de 6 à 11 ans en proposant, en collaboration avec les enseignants, des
-          actions sur-mesure et des supports pédagogiques ludiques d’éducation par le sport.
+          actions sur-mesure et des supports pédagogiques ludiques d’éducation par le sport.<br><br>
+          Nous sommes convaincus qu'il existe plusieurs manières de réussir, des parcours différents, des scolarités
+          variées qui permettent à tout un chacun de se réaliser. Il s'agit ici lutter contre le déterminisme scolaire
+          et de sensibiliser la jeunesse sur le fait que l’on peut avoir des qualités insoupçonnées, qui ne demandent
+          qu’à être transposées dans son parcours scolaire et mises à profit de tout au long de sa vie.
         </div>
         <div class="row">
           <div class="col-4">
@@ -190,7 +209,7 @@
               <div>
                 Des ateliers éducatifs et culturels autour d’ouvrages jeunesse sur les thèmes du sport et de ses
                 valeurs, nous proposons des exploitations pédagogiques développées avec les enseignants et des
-                concours d’albums jeunesse par équipes (écriture et illustrations).<br>
+                concours d’albums jeunesse par équipes (écriture et illustrations).<br><br>
                 #APPRENDRE<br>
                 #IMAGINER<br>
                 #CREER
@@ -205,7 +224,7 @@
               </div>
               <div>
                 Plusieurs ateliers dynamiques alliant des temps de pratiques d'initiations sportive et des activités
-                éducatives : dictées sportives, mental-run...<br>
+                éducatives : dictées sportives, mental-run...<br><br>
                 #BOUGER<br>
                 #S'AMUSER<br>
                 #S'EVEILLER
@@ -221,12 +240,38 @@
               <div>
                 Des athlètes de haut niveau interviennent pour transmettre aux enfants les clés de leur parcours. Ces
                 échanges sont aussi l'occasion d'aborder des sujets de société et de citoyenneté en rapport avec le
-                sport : la santé, l'inclusion, la mixité hommes/femmes, la lutte contre les discriminations...<br>
+                sport : la santé, l'inclusion, la mixité hommes/femmes, la lutte contre les discriminations...<br><br>
                 #INSPIRER<br>
                 #TRANSMETTRE
               </div>
             </div>
           </div>
+        </div>
+        <div class="app__section__title text-uppercase text-primary">
+          Dans nos tuyaux
+        </div>
+        <div class="row">
+          Nous souhaitons développer et offrir d’autres activités pour apprendre en s’amusant autour du sport et de ses valeurs :
+          <ul class="text-left">
+            <li>
+              Réaliser et mettre à disposition des carnets pédagogiques interdisciplinaires « clé en main » adaptés par
+              tranche d’âge et développés avec des enseignants et ainsi proposer aux éducateurs et parents des outils
+              d’éducation par le sport articulés avec les programmes scolaires. Il s’agit d’offrir du matériel
+              complémentaire, original et ludique aux enseignements fondamentaux : apprentissage de la lecture, de
+              l’écriture, des mathématiques, découverte du monde, anglais, éducation morale et civique... grâce au sport !
+            </li>
+            <li>
+              Créer et animer un réseau dans les quartiers prioritaires de la politique de la ville.
+            </li>
+            <li>
+              Créer une boîte à outils pédagogiques et ludiques et diffuser les bonnes pratiques en matière d’éducation par
+              le sport.
+            </li>
+            <li>
+              Établir un maximum de ponts avec les personnes et les associations qui oeuvrent déjà dans le domaine
+              de l’éducation par le sport.
+            </li>
+          </ul>
         </div>
       </div>
     </section>
@@ -286,23 +331,6 @@
         </div>
       </div>
     </section>
-    <section class="app__section app__section__odd row justify-content-md-center bg-warning" ref="section_quote">
-      <div class="col-6 text-center">
-        <div class="app__section__image mb-5">
-          <font-awesome-icon icon="quote-right" size="10x"></font-awesome-icon>
-        </div>
-        <div class="app__section__content font-weight-bold mt-3">
-          « L'éducation est une arme puissante pour faire évoluer les mentalités et transcender les différences,
-          et le sport est une source d'inspiration, de dépassement, de tolérance et d'apprentissage
-          du respect de la jeunesse.<br><br>
-          Ces deux éléments participent à créer
-          une société plus juste et fraternelle.»
-        </div>
-        <div class="app__section__author font-italic mt-3 font-weight-lighter">
-          Nelson Mandela
-        </div>
-      </div>
-    </section>
     <section class="app__section app__section__even row" ref="section_join">
       <div class="col-12">
         <div class="app__section__title text-uppercase text-center text-primary">
@@ -311,36 +339,14 @@
         <div class="app__section__cards__support mt-3">
           <b-card-group deck>
             <b-card
-                title="Je parraine des ateliers"
-                img-src="./assets/hand.png"
-                img-alt="Parrainage"
-                img-top
-                body-class="py-5 bg-dark text-light text-center">
-              <b-card-text>
-                Et j'agis positivement sur la vie locale!
-                <a href="" class="stretched-link"></a>
-              </b-card-text>
-            </b-card>
-            <b-card
                 title="J'adhère"
                 img-src="./assets/five.png"
                 img-alt="Adhésion"
                 img-top
-                body-class="py-5 bg-primary text-light text-center">
+                body-class="py-5 bg-danger text-light text-center">
               <b-card-text>
                 Et je participe à la vie de l'association!
                 <a href="https://www.helloasso.com/associations/les-carnets-de-jo" target="_blank" class="stretched-link"></a>
-              </b-card-text>
-            </b-card>
-            <b-card
-                title="Je contribue"
-                img-src="./assets/family.png"
-                img-alt="Temps"
-                img-top
-                body-class="py-5 bg-danger text-light text-center">
-              <b-card-text>
-                Et j'anime des ateliers ou j'apporte mon expertise pédagogique.
-                <a href="" class="stretched-link"></a>
               </b-card-text>
             </b-card>
             <b-card
@@ -348,13 +354,33 @@
                 img-src="./assets/family.png"
                 img-alt="Don"
                 img-top
-                body-class="py-5 bg-dark text-light text-center">
+                body-class="py-5 bg-danger text-light text-center">
               <b-card-text>
-                Et je permets le développement de l'association
+                Et je permets le développement des programmes de l'association
                 <a href="https://www.helloasso.com/associations/les-carnets-de-jo" target="_blank" class="stretched-link"></a>
               </b-card-text>
             </b-card>
           </b-card-group>
+        </div>
+        <div>
+          Une démarche collective et collaborative
+
+          Les "carnets de Jo" sont le fruit d'un travail collaboratif avec des enseignants et des conseillers
+          pédagogiques ou sportifs. Si vous aussi, vous souhaitez apporter votre contribution à nos prochains projets
+          pédagogiques, contactez-nous !
+        </div>
+        <div>
+          <b-card
+              title="Je contribue"
+              img-src="./assets/family.png"
+              img-alt="Temps"
+              img-top
+              body-class="py-5 bg-danger text-light text-center">
+            <b-card-text>
+              Et j'anime des ateliers ou j'apporte mon expertise pédagogique.
+              <a href="" class="stretched-link"></a>
+            </b-card-text>
+          </b-card>
         </div>
       </div>
     </section>
@@ -408,24 +434,14 @@
       </div>
     </section>
     <footer class="app__footer row bg-warning text-center mt-5 p-3">
-      <div class="col-4">
+      <div class="col-12">
         <img src="./assets/logo-footer.png" alt="Jo" width="50" class="app__footer__logo">
         LES CARNETS DE JO<br><br>
         6 rue Jean-Baptiste Dumay<br>
         75020 PARIS<br>
         FRANCE<br><br>
-        lescarnetsdejo@gmail.com
-      </div>
-      <div class="col-4">
+        lescarnetsdejo@gmail.com<br><br>
         ©2021 par <span class="font-italic">Les Carnets de Jo</span>
-      </div>
-      <div class="col-4">
-        <a href="https://www.facebook.com/CollectionJo/" target="_blank">
-          <font-awesome-icon :icon="['fab', 'facebook-square']" size="2x"></font-awesome-icon>
-        </a>
-        <a href="https://www.instagram.com/lesincroyablesrencontresdejo/" target="_blank" class="ml-2">
-          <font-awesome-icon :icon="['fab', 'instagram-square']" size="2x"></font-awesome-icon>
-        </a>
       </div>
     </footer>
   </div>
@@ -520,17 +536,11 @@ export default {
         margin: auto;
       }
     }
-
-    &__wheelchair {
-      position: absolute;
-      right: -350px;
-      top: 100px
-    }
   }
 
   &__footer {
     &__logo {
-      left: 30px;
+      left: 530px;
       position: absolute;
       top: 59px;
     }
